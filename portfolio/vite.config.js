4f41994936771918e17dfd3e582e7ecd66179853
harshitha <harshithamr461@gmail.com>
2025-08-13 09:09:19 +0530

    Ready for deployment

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react()],
   build: {
-    outDir: 'build',  // change output directory to 'build'
+    outDir: 'build',
   },
 })
